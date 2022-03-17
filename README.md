ce2d02a91efad5a76a000ea5578edac7b4446e5c
DonnieDice <DonnieDice@ProtonMail.com>
2022-03-16 23:44:08 -0500

    v1.0.2
    Muted WoW default level up sound

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 
 SRLU - Skyrim Level Up!
 
+
 This is an addon that will play the "LEVEL UP!" chime from Skyrim when you level up in WoW.
 
+
 This addon works in SHADOWLANDS!
+
 This addon works in CLASSIC WOW!
+
 This addon works in CLASSIC TBC!
+
+
+v1.0.2
+
+Added - Muted WoW default level up sound
--- main.lua
+++ main.lua
@@ -1,6 +1,15 @@
+-- Event function level up
 local a = CreateFrame("Frame")
 a:RegisterEvent("PLAYER_LEVEL_UP")
 a:SetScript("OnEvent",
 	function(self, event, ...)
 		PlaySoundFile("Interface\\Addons\\SRLU\\SRLU.ogg", "Master")
+	end)
+
+-- Mute default level up sound
+local b = CreateFrame("Frame")
+b:RegisterEvent("ADDON_LOADED")
+b:SetScript("OnEvent",
+	function(self, event, ...)
+		MuteSoundFile(569593)
 	end)
